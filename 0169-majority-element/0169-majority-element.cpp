16ff7138495fb2ada8f2ca7eafaab4ab471a8011
Roshan <43458444+roshankumarsinha@users.noreply.github.com>
2024-10-21 21:13:30 +0530

    Time: 0 ms (100%), Space: 27 MB (79.79%) - LeetHub

---
+++ 0169-majority-element/0169-majority-element.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    int majorityElement(vector<int>& nums) {
+        // At the end majority element count will always be +ve as it appears move than n/2
+
+        int candidate = 0;
+        int count = 0;
+        for (int value:nums) {
+            if (count == 0) {
+                candidate = value;
+            }
+            if (candidate == value) {
+                count++;
+            }
+            else {
+                count--;
+            }
+        }
+        return candidate;
+    }
+};
