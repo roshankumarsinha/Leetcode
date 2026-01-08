a73bf53a215db6dbf2d10c80c9ab222a8e449c78
Roshan <43458444+roshankumarsinha@users.noreply.github.com>
2026-01-08 14:40:47 +0530

    Time: 0 ms (100%), Space: 12.8 MB (76.68%) - LeetHub

--- 0100-same-tree/0100-same-tree.cpp
+++ 0100-same-tree/0100-same-tree.cpp
@@ -6,56 +6,61 @@
  *     TreeNode *right;
  *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
  *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
- *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left),
+ * right(right) {}
  * };
  */
 class Solution {
 public:
-    bool isSameTree(TreeNode* p, TreeNode* q) {
-        if (p == nullptr && q == nullptr) {
+    bool isSameTree(TreeNode* r1, TreeNode* r2) {
+        // If both are null then return true, as empty tree are considered
+        // identical
+        if (r1 == nullptr && r2 == nullptr)
             return true;
-        }
-        if (p == nullptr && q != nullptr) {
+
+        // If any one of the tree is null and other one not then return false
+        if (r1 == nullptr || r2 == nullptr)
             return false;
+
+        // Use two queues for level order traversal
+        queue<TreeNode*> q1, q2;
+        q1.push(r1);
+        q2.push(r2);
+
+        while (!q1.empty() && !q2.empty()) {
+            TreeNode* node1 = q1.front();
+            TreeNode* node2 = q2.front();
+            q1.pop();
+            q2.pop();
+
+            // Check if the current nodes are identical
+            if (node1->val != node2->val)
+                return false;
+
+            // Check the left children
+            if (node1->left && node2->left) {
+                q1.push(node1->left);
+                q2.push(node2->left);
+            } else if (node1->left ||
+                       node2->left) { // If one has left subtree, but other
+                                      // doesn't have then, they are not
+                                      // identical
+                return false;
+            }
+
+            // Check the right children
+            if (node1->right && node2->right) {
+                q1.push(node1->right);
+                q2.push(node2->right);
+            } else if (node1->right ||
+                       node2->right) { // If one has right subtree, but other
+                                       // doesn't have then, they are not
+                                       // identical
+                return false;
+            }
         }
-        if (p != nullptr && q == nullptr) {
-            return false;
-        }
-        queue<TreeNode*> pQ;
-        queue<TreeNode*> qQ;
-        pQ.push(p);
-        qQ.push(q);
-        while(!pQ.empty() && !qQ.empty()) {
-            TreeNode* pCurr = pQ.front();
-            TreeNode* qCurr = qQ.front();
-            cout<<pCurr->val<<" "<<qCurr->val<<"\n";
-            pQ.pop();
-            qQ.pop();
-            if (pCurr->val != qCurr->val) {
-                return false;
-            }
-            if (pCurr->left && qCurr->left) {
-                pQ.push(pCurr->left);
-                qQ.push(qCurr->left);
-            }
-            if (pCurr->right && qCurr->right) {
-                pQ.push(pCurr->right);
-                qQ.push(qCurr->right);
-            }
-            if (pCurr->left && !qCurr->left) {
-                return false;
-            }
-            if (!pCurr->left && qCurr->left) {
-                return false;
-            }
-            if (pCurr->right && !qCurr->right) {
-                return false;
-            }
-            if (!pCurr->right && qCurr->right) {
-                return false;
-            }
-        }
-        return pQ.empty() && qQ.empty();
-        
+
+        // If both queues are empty, the trees are identical
+        return q1.empty() && q2.empty();
     }
 };
