c0865f72b1c5502ee5a83a267e26ea04e0777455
Roshan <43458444+roshankumarsinha@users.noreply.github.com>
2026-01-12 20:12:24 +0530

    Time: 8 ms (54.97%), Space: 11.9 MB (53.25%) - LeetHub

--- 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.cpp
+++ 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.cpp
@@ -1,23 +1,18 @@
 class Solution {
 public:
     int lengthOfLongestSubstring(string s) {
-        vector<int> charIndex(256, -1); // Hash map to store the last index of each character
-    int left = 0; // Left pointer of the sliding window
-    int maxLength = 0; // Maximum length of substring found
-
-    // Expand the window with the right pointer
-    for (int right = 0; right < s.length(); right++) {
-        char currentChar = s[right];
-
-        // If the character has been seen and is within the current window
-        if (charIndex[currentChar] >= left) {
-            left = charIndex[currentChar] + 1; // Move left pointer to the right of the last occurrence
+        unordered_map<char, pair<int, int>> fm;
+        int left = 0;
+        int maxi = 0;
+        for (int right = 0; right < s.size(); right++) {
+            char c = s[right];
+            if (fm[c].first !=0 && fm[c].second >= left) {
+                left = fm[c].second + 1;
+            }
+            int val = fm[c].first + 1;
+            fm[c] = {val, right};
+            maxi = max(maxi, right - left + 1);
         }
-
-        charIndex[currentChar] = right; // Update the last index of the current character
-        maxLength = max(maxLength, right - left + 1); // Update maxLength if needed
-    }
-
-    return maxLength;
+        return maxi;
     }
 };
