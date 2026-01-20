6a763115b24c0cc296ebee61e098159eb16a7ba0
Roshan <43458444+roshankumarsinha@users.noreply.github.com>
2026-01-20 21:05:34 +0530

    Time: 0 ms (100%), Space: 28.1 MB (86.36%) - LeetHub

--- 0169-majority-element/0169-majority-element.cpp
+++ 0169-majority-element/0169-majority-element.cpp
@@ -1,33 +1,17 @@
 class Solution {
 public:
     int majorityElement(vector<int>& nums) {
-        // At the end majority element count will always be +ve as it appears move than n/2.
-        // This algo. is called Moore's Algorithm
-        int candidate = 0;
+        int candidate = INT_MIN;
         int count = 0;
-        for (int value:nums) {
-            if (count == 0) {
-                candidate = value;
-            }
-            if (candidate == value) {
+        for (int val : nums) {
+            if (val == candidate) {
                 count++;
-            }
-            else {
+            } else if (count == 0) {
+                candidate = val;
+            } else {
                 count--;
             }
         }
         return candidate;
-
-        // If it is not given that there definitely exists a number > n/2 times frequency, in that case return -1
-        // int count = 0;
-        // for (int value:nums) {
-        //     if (value == candidate) {
-        //         count++;
-        //     }
-        // }
-        // if (count > n/2) {
-        //     return candidate;
-        // }
-        // else return -1;
     }
 };
