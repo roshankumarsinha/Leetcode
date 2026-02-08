3729cb5843838e587a6dcc3d70525905c5690bde
Roshan <43458444+roshankumarsinha@users.noreply.github.com>
2026-02-08 15:39:52 +0530

    Time: 0 ms (100%), Space: 54.2 MB (33.39%) - LeetHub

---
+++ 0796-rotate-string/0796-rotate-string.js
@@ -0,0 +1,12 @@
+/**
+ * @param {string} s
+ * @param {string} goal
+ * @return {boolean}
+ */
+var rotateString = function (s, goal) {
+    if (s.length !== goal.length) {
+        return false;
+    }
+    let newStr = s + s;
+    return newStr.includes(goal);
+};
